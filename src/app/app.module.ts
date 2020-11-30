2cc9c75305e6921fd07cc61d5266ed5584729f8b
nanokozmos <ovuncuzun@gmail.com>
2020-11-30 14:25:17 +0000

    updating app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoryFeedComponent } from './components/story-feed/story-feed.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { StoryItemComponent } from './components/story-feed/story-item/story-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StoryFeedComponent
+    StoryFeedComponent,
+    StoryItemComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
